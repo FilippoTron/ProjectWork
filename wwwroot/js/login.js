7e472fada750eec0af0b745570319327c35089b9
Filippo TRON <filippo.tron@saponemail.com>
2025-08-06 16:14:40 +0200

    bug fixes

---
+++ wwwroot/js/login.js
@@ -0,0 +1,43 @@
+﻿const loginForm = document.getElementById("loginForm");
+if (loginForm) {
+
+    loginForm.addEventListener("submit", async e => {
+        e.preventDefault();
+
+        const username = document.getElementById("loginUsername").value;
+        const password = document.getElementById("loginPassword").value;
+
+        const btn = document.getElementById("loginBtn");
+        const spinner = document.getElementById("loginSpinner");
+        const btnText = document.getElementById("loginBtnText");
+        const msg = document.getElementById("loginMsg");
+
+        btn.disabled = true;
+        spinner.classList.remove("d-none");
+        btnText.textContent = "Accesso...";
+
+        try {
+            const res = await fetch(`${apiUrl}/api/auth/login`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, password })
+            });
+
+            if (res.ok) {
+                const data = await res.json();
+                localStorage.setItem("token", data.token);
+                const payload = parseJwt(data.token);
+                window.location.href = payload.role === "Admin" ? "admin.html" : "loan.html";
+            } else {
+                msg.textContent = "Credenziali non valide.";
+            }
+        } catch (err) {
+            msg.textContent = "Errore durante il login.";
+            console.error(err);
+        }
+
+        btn.disabled = false;
+        spinner.classList.add("d-none");
+        btnText.textContent = "Login";
+    });
+}
---
+++ wwwroot/js/register.js
@@ -0,0 +1,25 @@
+﻿const registerForm = document.getElementById("registerForm");
+if (registerForm) {
+
+    registerForm.addEventListener("submit", async e => {
+        e.preventDefault();
+
+        const u = document.getElementById("registerUsername").value;
+        const em = document.getElementById("registerEmail").value;
+        const p = document.getElementById("registerPassword").value;
+
+        const res = await fetch(`${apiUrl}/api/Auth/register`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username: u, email: em, password: p })
+        });
+
+        const msg = document.getElementById("registerMsg");
+        if (res.ok) {
+            msg.textContent = "Registrato! Vai a login...";
+            setTimeout(() => window.location.href = "login.html", 1000);
+        } else {
+            msg.textContent = "Registrazione fallita.";
+        }
+    });
+}
